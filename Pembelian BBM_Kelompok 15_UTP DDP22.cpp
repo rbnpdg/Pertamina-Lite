dc2818b975919e2d4f7f4c5be1fb21bab780d7b0
rbnpdg <112568840+rbnpdg@users.noreply.github.com>
2022-10-20 13:39:59 +0700

    Update Pembelian BBM_Kelompok 15_UTP DDP22.cpp

--- Pembelian BBM_Kelompok 15_UTP DDP22.cpp
+++ Pembelian BBM_Kelompok 15_UTP DDP22.cpp
@@ -29,11 +29,11 @@ int proses() {
 	int harga[10] = {10000, 14500, 16250, 17750};
 	
 	do {
-		system ("color 5");
-		system("CLS");
-		header();
-		cout << endl;
-		cout << "Jenis BBM yang tersedia disini :" << endl;
+	    system ("color 5");
+	    system("CLS");
+	    header();
+	    cout << endl;
+	    cout << "Jenis BBM yang tersedia disini :" << endl;
 	    cout << "______________________________________________\n";
 	    cout << " Kode |     Jenis BBM     |  Harga/Liter\n";
 	    cout << "______________________________________________\n";
@@ -67,7 +67,7 @@ int proses() {
 	        cout << "Masukkan metode pembayaran : ";
 	        cin >> bayar;
 	 	       
-	 	       cout << endl;
+	 	cout << endl;
 	 	       
 	        if(bayar == 1) { //jika memilih cash maka akan langsung keluar struk pembelian
 	            cout << "-----Transaksi Anda berhasil!-----\n";
@@ -77,37 +77,35 @@ int proses() {
 	            cout << "Jenis BBM\t: Pertalite\n";
 	            cout << "Jumlah\t\t: " << pertalite << " liter\n";
 	            cout << "Total\t\t: Rp. " << uang << endl;
-	               cout << "__________________________________\n";
+	            cout << "__________________________________\n";
 	       		
 	       		
 	        } else if( bayar == 2) { // Akan muncul nota untuk pembayaran yang disertai dengan pajak 0,2%
-	        	system ("CLS");
-	        	cout << "==================================================\n";
-	        	cout << "Volume pertalite\t: " << pertalite << " liter\n";
-	        	cout << "Harga\t\t\t: Rp. " << uang << endl;
-	        	cout << "Pajak\t\t\t: Rp. " << pajak << endl;
-	        	cout << "Total\t\t\t: Rp. " << uang + pajak << endl;
+	            system ("CLS");
+	            cout << "==================================================\n";
+	            cout << "Volume pertalite\t: " << pertalite << " liter\n";
+	            cout << "Harga\t\t\t: Rp. " << uang << endl;
+	            cout << "Pajak\t\t\t: Rp. " << pajak << endl;
+	            cout << "Total\t\t\t: Rp. " << uang + pajak << endl;
 	        	
-	        	//Kode transaksi ketika menggunakan dana akan keluar
-	        	cout << "Kode transaksi dana kamu adalah '1411'\n";
-	        	cout << "Masukan kode transaksi yang kamu dapatkan : ";
-	        	cin >> verif;
+	            //Kode transaksi ketika menggunakan dana akan keluar
+	            cout << "Kode transaksi dana kamu adalah '1411'\n";
+	            cout << "Masukan kode transaksi yang kamu dapatkan : ";
+	            cin >> verif;
 	        	
-	        	cout << endl;
+	            cout << endl;
 	        	
-	        	if(verif == 1411){ // Jika sudah memasukan kode transaksi dana akan keluar struk pembelian
-	        	   cout << "==========e-SPBU Invoice==========\n\n";
+	            if(verif == 1411){ // Jika sudah memasukan kode transaksi dana akan keluar struk pembelian
+	               cout << "==========e-SPBU Invoice==========\n\n";
 	               cout << "Jenis BBM\t: Pertalite\n";
 	               cout << "Jumlah\t\t: " << pertalite << " liter\n";
 	               cout << "Total\t\t: Rp. " << uang + pajak << endl;
 	               cout << "__________________________________\n";
-				} else { // Jika memasukan kode selain 1411 makan akan tidak valid
-					cout << "Mohon maaf transaksi Anda dibatalkan karena kode tidak valid\n";
-				}
-			}
-	   	} 
-	
-			else if(pilihan == 2) { //Jenis BBM Pertamax
+		    } else { // Jika memasukan kode selain 1411 makan akan tidak valid
+			cout << "Mohon maaf transaksi Anda dibatalkan karena kode tidak valid\n";
+		    }
+		}
+	     } else if(pilihan == 2) { //Jenis BBM Pertamax
 	    	//PEMESANAN
 	        cout << "Masukkan jumlah pembelian Pertamax Anda\n";
 	        cout << "Rp. ";cin >> uang;
@@ -126,105 +124,98 @@ int proses() {
 	        // Pemilihan Metode Pembayaran
 	        cout << "Pilih metode pembayaran\n[1]Cash\n[2]Dana\n";
 	        cout << "Masukkan metode pembayaran : ";
-			cin >> bayar;
+		cin >> bayar;
 	 	    
-	 	    cout << endl;
+	 	cout << endl;
 	 	    
-	         if(bayar == 1) { //jika memilih cash maka akan langsung keluar struk pembelian
-	            cout << "-----Transaksi Anda berhasil!-----\n";
+	        if(bayar == 1) { //jika memilih cash maka akan langsung keluar struk pembelian
+	           cout << "-----Transaksi Anda berhasil!-----\n";
+	           cout << "==========e-SPBU Invoice==========\n\n";
+	           cout << "Jenis BBM\t: Pertamax\n";
+	           cout << "Jumlah\t\t: " << pertamax << " liter\n";
+	           cout << "Total\t\t: Rp. " << uang << endl;
+	           cout << "__________________________________\n";
+	        } else if( bayar == 2) { // Akan muncul nota untuk pembayaran yang disertai dengan pajak 0,2%
+		   system ("CLS");
+	           cout << "Volume BBM\t: " << pertamax << " liter\n";
+	           cout << "Harga\t\t: Rp. " << uang << endl;
+	           cout << "Pajak\t\t: Rp. n" << pajak << endl;
+	           cout << "Total\t\t: Rp. " << uang + pajak << endl;
+	        	
+	           // Kode transaksi ketika menggunakan dana akan keluar
+	           cout << "Kode transaksi dana kamu adalah '1411'\n";
+	           cout << "Masukan kode transaksi yang kamu dapatkan : ";
+	           cin >> verif;
 	        
-	            
-	            cout << "==========e-SPBU Invoice==========\n\n";
-	            cout << "Jenis BBM\t: Pertamax\n";
-	            cout << "Jumlah\t\t: " << pertamax << " liter\n";
-	            cout << "Total\t\t: Rp. " << uang << endl;
-	               cout << "__________________________________\n";
-	        }
-			else if( bayar == 2) { // Akan muncul nota untuk pembayaran yang disertai dengan pajak 0,2%
-				system ("CLS");
-	        	cout << "Volume BBM\t: " << pertamax << " liter\n";
-	        	cout << "Harga\t\t: Rp. " << uang << endl;
-	        	cout << "Pajak\t\t: Rp. n" << pajak << endl;
-	        	cout << "Total\t\t: Rp. " << uang + pajak << endl;
+	           cout << endl;
 	        	
-	        	// Kode transaksi ketika menggunakan dana akan keluar
-	        	cout << "Kode transaksi dana kamu adalah '1411'\n";
-	        	cout << "Masukan kode transaksi yang kamu dapatkan : ";
-	        	cin >> verif;
-	        	
-	        	cout << endl;
-	        	
-	        	if(verif == 1411){ // Jika sudah memasukan kode transaksi dana akan keluar struk pembelian
-	        	   cout << "==========e-SPBU Invoice==========\n\n";
+	           if(verif == 1411){ // Jika sudah memasukan kode transaksi dana akan keluar struk pembelian
+	               cout << "==========e-SPBU Invoice==========\n\n";
 	               cout << "Jenis BBM\t: Pertamax\n";
 	               cout << "Jumlah\t\t: " << pertamax << " liter\n";
 	               cout << "Total\t\t: Rp. " << uang + pajak << endl;
 	               cout << "__________________________________\n";
-				} else { // Jika memasukan kode selain 1411 makan akan tidak valid
-					cout << "Mohon maaf transaksi Anda dibatalkan karena kode tidak valid\n";
-				}
-			}
-		} 
-			
-			else if(pilihan == 3) { // Jenis BBM Pertamax
-	    	//PEMESANAN
-	        cout << "Masukkan jumlah pembelian Pertamax Turbo Anda\n";
-	        cout << "Rp. ";cin >> uang;
-	        
-	        if(uang < 1) { // kasus terjadi jika nominal rupiah yang dimasukkan kurang dari Rp. 1
-	            cout << "Nominal tidak valid, silahkan masukkan nominal pembelian kembali\n";
-	            cout << "Masukkan jumlah pembelian Pertamax Anda\n";
-	            cout << "Rp. ";cin >> uang;
+		   } else { // Jika memasukan kode selain 1411 makan akan tidak valid
+		       cout << "Mohon maaf transaksi Anda dibatalkan karena kode tidak valid\n";
+		   }
 	        }
+	     } else if(pilihan == 3) { // Jenis BBM Pertamax
+	    	   //PEMESANAN
+	           cout << "Masukkan jumlah pembelian Pertamax Turbo Anda\n";
+	           cout << "Rp. ";cin >> uang;
 	        
-	        //penghitungan per liter yang didapat dan pajak jika menggunakan pembayaran dana
-	        pertabo = uang/16250;
-	        pajak = 1000;
+	           if(uang < 1) { // kasus terjadi jika nominal rupiah yang dimasukkan kurang dari Rp. 1
+	               cout << "Nominal tidak valid, silahkan masukkan nominal pembelian kembali\n";
+	               cout << "Masukkan jumlah pembelian Pertamax Anda\n";
+	               cout << "Rp. ";cin >> uang;
+	           }
 	        
-	        cout << endl;
-	       // Pemilihan Metode Pembayaran
-	        cout << "Pilih metode pembayaran\n[1]Cash\n[2]Dana\n";
-	        cout << "Masukkan metode pembayaran : ";
-			cin >> bayar;
+	           //penghitungan per liter yang didapat dan pajak jika menggunakan pembayaran dana
+	           pertabo = uang/16250;
+	           pajak = 1000;
+	        
+	           cout << endl;
+	           // Pemilihan Metode Pembayaran
+	           cout << "Pilih metode pembayaran\n[1]Cash\n[2]Dana\n";
+	           cout << "Masukkan metode pembayaran : ";
+		   cin >> bayar;
 	 	    
-	 	    cout << endl;
+	 	   cout << endl;
 	 	    
-	        if(bayar == 1) { //jika memilih cash maka akan langsung keluar struk pembelian
-	            cout << "-----Transaksi Anda berhasil!-----\n";
+	           if(bayar == 1) { //jika memilih cash maka akan langsung keluar struk pembelian
+	               cout << "-----Transaksi Anda berhasil!-----\n";
 	        
-	            cout << "==========e-SPBU Invoice==========\n\n";
-	            cout << "Jenis BBM\t: Pertamax Turbo\n";
-	            cout << "Jumlah\t\t: " << pertabo << " liter\n";
-	            cout << "Total\t\t: Rp. " << uang << endl;
-	               cout << "__________________________________\n";
-	        }
-	    
-			else if( bayar == 2) { // Akan muncul nota untuk pembayaran yang disertai dengan pajak 0,2%
-				system ("CLS");
-	        	cout << "Volume BBM\t: " << pertabo << " liter\n";
-	        	cout << "Harga\t\t: Rp. " << uang << endl;
-	        	cout << "Pajak\t\t: Rp. " << pajak << endl;
-	        	cout << "Total\t\t: Rp. " << uang + pajak << endl;
-	        	
-	        	// Kode transaksi ketika menggunakan dana akan keluar
-	        	cout << "Kode transaksi dana kamu adalah '1411'\n";
-	        	cout << "Masukan kode transaksi yang kamu dapatkan : ";
-	        	cin >> verif;
-	        	
-	        	cout << endl;
-	        	
-	        	if(verif == 1411){ // Jika sudah memasukan kode transaksi dana akan keluar struk pembelian
-	        	   cout << "==========e-SPBU Invoice==========\n\n";
+	               cout << "==========e-SPBU Invoice==========\n\n";
 	               cout << "Jenis BBM\t: Pertamax Turbo\n";
 	               cout << "Jumlah\t\t: " << pertabo << " liter\n";
-	               cout << "Total\t\t: Rp. " << uang + pajak << endl;
+	               cout << "Total\t\t: Rp. " << uang << endl;
 	               cout << "__________________________________\n";
-				} else { // Jika memasukan kode selain 1411 makan akan tidak valid
-					cout << "Mohon maaf transaksi Anda dibatalkan karena kode tidak valid\n";
-				}
+	           } else if( bayar == 2) { // Akan muncul nota untuk pembayaran yang disertai dengan pajak 0,2%
+		       system ("CLS");
+	               cout << "Volume BBM\t: " << pertabo << " liter\n";
+	               cout << "Harga\t\t: Rp. " << uang << endl;
+	               cout << "Pajak\t\t: Rp. " << pajak << endl;
+	               cout << "Total\t\t: Rp. " << uang + pajak << endl;
+	        	
+	               // Kode transaksi ketika menggunakan dana akan keluar
+	               cout << "Kode transaksi dana kamu adalah '1411'\n";
+	               cout << "Masukan kode transaksi yang kamu dapatkan : ";
+	               cin >> verif;
+	        	
+	               cout << endl;
+	        	
+	               if(verif == 1411){ // Jika sudah memasukan kode transaksi dana akan keluar struk pembelian
+	        	   cout << "==========e-SPBU Invoice==========\n\n";
+	                   cout << "Jenis BBM\t: Pertamax Turbo\n";
+	                   cout << "Jumlah\t\t: " << pertabo << " liter\n";
+	                   cout << "Total\t\t: Rp. " << uang + pajak << endl;
+	                   cout << "__________________________________\n";
+			} else { // Jika memasukan kode selain 1411 makan akan tidak valid
+		           cout << "Mohon maaf transaksi Anda dibatalkan karena kode tidak valid\n";
 			}
+		    }
 
-		} 
+	      } 
 			// pembelian untuk Pertamina Dex
 			else if(pilihan == 4) {
 	    	//PEMESANAN
